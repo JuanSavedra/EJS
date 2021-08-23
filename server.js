61e666e19048f2c452e1c7fef7125747b2fa5798
Juan Savedra dos Santos <juansavedra827@gmail.com>
2021-08-23 11:48:39 -0300

    add foreach of objects and finish project

--- server.js
+++ server.js
@@ -4,7 +4,30 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    res.render("pages/index");
+    const items = [
+        {
+            title: "J",
+            message: "uan"
+        },
+        {
+            title: "S",
+            message: "avedra"
+        },
+        {
+            title: "d",
+            message: "os"
+        },
+        {
+            title: "S",
+            message: "antos"
+        }
+    ];
+
+    const description = "";
+    res.render("pages/index", {
+        qualitys: items, 
+        description: "Linguagem de Programação Front-End que usa NodeJS como Back-End."
+    });
 });
 
 app.get("/sobre", function (req, res) {
